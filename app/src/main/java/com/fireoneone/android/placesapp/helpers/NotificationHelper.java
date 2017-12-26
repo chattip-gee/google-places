24e87fe04a2f81667f223fe6ee70199f49fecdd8
Chattip Soontaku <gee.c@excelbangkok.com>
2017-12-26 22:48:20 +0700

    Fixed notification support android 8 Oreo.

--- app/src/main/java/com/fireoneone/android/placesapp/helpers/NotificationHelper.java
+++ app/src/main/java/com/fireoneone/android/placesapp/helpers/NotificationHelper.java
@@ -1,10 +1,13 @@
 package com.fireoneone.android.placesapp.helpers;
 
-import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 
 import com.fireoneone.android.placesapp.R;
 import com.fireoneone.android.placesapp.activities.NavigationActivity;
@@ -16,24 +19,34 @@ import static android.content.Context.NOTIFICATION_SERVICE;
  */
 
 public class NotificationHelper {
+    private static final String NOTIFICATION_CHANNEL_ID = "com.fireoneone.android.placesapp";
 
     public static void notificationMessage(Context context, String title, String message, int notificationId) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationChannel.setDescription("Channel Places App");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(context.getResources().getColor(R.color.standard_app_color));
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
         Intent intent = new Intent(context, NavigationActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, notificationId, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        Notification notification = new Notification.Builder(context)
-                .setSmallIcon(R.mipmap.ic_launcher)
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                .setVibrate(new long[]{0, 100, 100, 100, 100, 100})
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setSmallIcon(R.drawable.ic_place)
                 .setContentTitle(title)
                 .setContentText(message)
                 .setContentIntent(pendingIntent)
-                .setAutoCancel(true)
-                .build();
-        notification.defaults |= Notification.DEFAULT_SOUND;
-        notification.defaults |= Notification.DEFAULT_LIGHTS;
-        notification.defaults |= Notification.DEFAULT_VIBRATE;
+                .setAutoCancel(true);
 
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.notify(notificationId, notification);
+        notificationManager.notify(notificationId, builder.build());
     }
 }
